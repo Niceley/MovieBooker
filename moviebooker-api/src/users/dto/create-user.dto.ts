318196901e963a16a7d8966f2d2878243f064348
David WU <david.wu@reseau.sncf.fr>
2025-04-10 11:51:24 +0200

    Code upgrade

--- moviebooker-api/src/users/dto/create-user.dto.ts
+++ moviebooker-api/src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsEmail, MinLength } from 'class-validator';
+import { IsString, IsEmail, MinLength, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
@@ -16,14 +16,21 @@ export class CreateUserDto {
     example: 'davidwu.pro@gmail.com',
     description: 'Adresse email',
   })
-  @IsEmail()
+  @IsEmail({}, { message: 'Veuillez entrer une adresse email valide' })
+  @Matches(
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    { message: 'Le format de l\'email n\'est pas valide (exemple: nom@domaine.com)' }
+  )
   email: string;
 
   @ApiProperty({
-    example: 'password123',
-    description: 'Mot de passe',
+    example: 'Password123!',
+    description: 'Mot de passe (minimum 8 caractères, au moins une majuscule, un chiffre et un caractère spécial)',
   })
   @IsString()
   @MinLength(8)
+  @Matches(/(?=.*[A-Z])/, { message: 'Le mot de passe doit contenir au moins une majuscule' })
+  @Matches(/(?=.*[0-9])/, { message: 'Le mot de passe doit contenir au moins un chiffre' })
+  @Matches(/(?=.*[!@#$%^&*])/, { message: 'Le mot de passe doit contenir au moins un caractère spécial (!@#$%^&*)' })
   password: string;
 }
