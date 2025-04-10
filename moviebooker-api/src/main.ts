beadb608b0a4cb1a4e93e0ecfa3ed552f1e1bb5d
David WU <david.wu@reseau.sncf.fr>
2025-04-10 11:31:51 +0200

    small fix on cors issue

--- moviebooker-api/src/main.ts
+++ moviebooker-api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://movie-booker-beta.vercel.app/',
+    origin: 'https://movie-booker-beta.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
